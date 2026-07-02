c61dd4e1eb342e4fc9b6ab976d2c39a0e2df7f30
SravaniMaragani27 <SravaniMaragani27>
2026-07-02 13:00:27 +0530

    changed the config file.

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,6 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  // i am creating it for vercel deployement.but it was  not  worked. 
   base:process.env.VITE_BASE_PATH || "/Ecommerce-Project",
 })
